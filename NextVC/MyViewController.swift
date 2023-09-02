feeb4215d44017f676a01eccddf9c05e61db6309
Jaesuk <jaesuk95@outlook.com>
2023-09-03 01:12:54 +0900

    creating page connection through storyboard

---
+++ NextVC/MyViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MyViewController.swift
+//  NextVC
+//
+//  Created by Jaesuk Lee on 2023/09/03.
+//
+
+import UIKit
+
+class MyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
--- NextVC/ViewController.swift
+++ NextVC/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         
     }
     
+    
+    
     // 1) 코드로 화면 이동 (다음화면이 코드로 작성되어있을때만 가능한 방법)
     @IBAction func codeNextButtonTapped(_ sender: UIButton) {
         let firstVC = FirstViewController()
@@ -27,12 +29,36 @@ class ViewController: UIViewController {
         present(firstVC, animated: true, completion: nil)
     }
     
+    
+    
+    
+    
     // 2) 코드로 스토리보드 객체를 생성해서, 화면 이동
     @IBAction func storyboardWithCodeButtonTapped(_ sender: UIButton) {
-
         
+        // 방법 1
+//        let secondVC = storyboard?.instantiateViewController(withIdentifier: "secondVC") as! SecondViewController // 실패하지 않을 자신 있으면 'as!'
+//        present(secondVC, animated: true, completion: nil)
         
+        // 방법 2
+//        if let secondVC = storyboard?.instantiateViewController(withIdentifier: "secondVC") as? SecondViewController {
+//            present(secondVC, animated: true, completion: nil)
+//        }
         
+        // 방법 3
+        guard let secondVC = storyboard?.instantiateViewController(withIdentifier: "secondVC") as? SecondViewController else {
+            // guard let, 성공하지 못하면 여기 클로저 안으로 들어온다
+            return
+        }
+        secondVC.modalPresentationStyle = .fullScreen
+        secondVC.someString = "storyboard setting"
+        
+        // 밑에 있는 코드는 왜 알될까? 생성자로 통해서 secondVC 를 만들었는데도 불구하고,
+        // 생성자와, storyboard 는 생성되는 순간이 다르다.
+        // 그래서 여기는 연결이 되기 전에 접근을 하고 있어, 에러가 나는거다.
+//        secondVC.mainLabel.text = "storyboard setting"  // 에러
+        
+        present(secondVC, animated: true, completion: nil)
         
     }
     
